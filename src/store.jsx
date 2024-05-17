1e7e3904b5607727ec7029c2a686d2875c1e1368
Vukkisila-Shobhitha <162134462+Vukkisila-Shobhitha@users.noreply.github.com>
2024-05-17 22:16:18 +0530

    Delete src/store.jsx'

--- src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { productSlice } from "./reducers/products";
-
-export const store = configureStore({
-  reducer: {
-    app: productSlice.reducer,
-  },
-});
